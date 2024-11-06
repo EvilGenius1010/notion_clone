8e6c3536900e31f328828fe5a2791a1922077e71
quirkyNashedi <harshavardhan.mitblr2022@learner.manipal.edu>
2024-11-06 12:42:26 +0530

    working zustand

--- main/src/store/modifiedPageContent.ts
+++ main/src/store/modifiedPageContent.ts
@@ -1,24 +1,42 @@
 //used for storing state of modified content of a page. 
 
+//
+// import { create } from "zustand"
+// interface StoreState {
+//   content: string;
+//   inc: () => void;
+// }
+// export default function useModifiedContent() {
+//   create<StoreState>()((set) => ({
+//     content: "",
+//     inc: (textToAdd: string) => set(state => ({ content: state.content + textToAdd }))
+//   }));
+//
+// }
+// //
+// // const modifiedContent = create((set) => {
+// //   content: "", //name of var and its content
+// //     inc: (textToAdd) => set((state) => ({ content: state.content + textToAdd }))
+// //
+// // })
+// //
 
-import { create } from "zustand"
-interface StoreState {
-  content: string;
-  inc: () => void;
+import { create } from "zustand";
+
+// We can use interface here as well
+type StateType = {
+  modifiedContent: string;     // the variable that we'll change using increment and decrement functions
+  setModContent: (someText: string) => void;
+  // both increment and decrement are anonymous functions that have void return type.
 }
-export default function useModifiedContent() {
-  create<StoreState>()((set) => ({
-    content: "",
-    inc: (textToAdd: string) => set(state => ({ content: state.content + textToAdd }))
-  }));
 
-}
-//
-// const modifiedContent = create((set) => {
-//   content: "", //name of var and its content
-//     inc: (textToAdd) => set((state) => ({ content: state.content + textToAdd }))
-//
-// })
-//
+const useModifiedContent = create<StateType>((set) => ({
+  // state variables
+  modifiedContent: "",
+  setModContent: (someText) =>
+    set((state: StateType) => ({
+      modifiedContent: state.modifiedContent + someText
+    })),
 
-
+}))
+export default useModifiedContent;
--- main/src/store/oldPageContent.ts
+++ main/src/store/oldPageContent.ts
@@ -1,1 +1,18 @@
 //store latest saved change.
+
+import { create } from "zustand";
+
+type OldContentType = {
+  content: string,
+  addLatestContent: (changes: string) => void
+}
+
+
+const useOldContent = create<OldContentType>((set) => ({
+  content: "",
+  addLatestContent: (changes => set((state: OldContentType) => ({ content: state.content + changes })
+  ))
+
+}))
+
+export default useOldContent
